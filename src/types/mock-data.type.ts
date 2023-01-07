564fee5a637f5744f888c13d9f09a23bb2cd753a
Sultan <sultan_galimov@bk.ru>
2023-01-07 10:01:26 +0500

    module2-taks1

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,18 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  postDates: string[];
+  towns: string[];
+  prices: string[];
+  usersId: string[];
+  previewImages: string[];
+  offerImages: string[];
+  premiumFlags: string[];
+  ratings: string[];
+  types: string[];
+  rooms: string[];
+  guests: string[];
+  coordinates: string[];
+  goods: string[];
+  commentsCount: string[];
+};
